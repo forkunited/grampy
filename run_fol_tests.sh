723379ce69d05488800a6806f665f767f7a3aa2a
Bill McDowell <wmcdowel@corn22.stanford.edu>
2017-05-10 14:52:16 -0700

    Fixed test script

--- run_fol_tests.sh
+++ run_fol_tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+TEST_DIR=src/test/py/fol
 PWD=`pwd` 
 export PYTHONPATH=$PYTHONPATH:${PWD}/../../../main/py/
 export PROVER9=<PATH TO PROVER BINARY>
 
-python test_fol_data.py
-python test_fol_rules.py
-python test_fol_model.py
+python ${TEST_DIR}/test_fol_data.py
+python ${TEST_DIR}/test_fol_rules.py
+python ${TEST_DIR}/test_fol_model.py
